20e72ea6575bd68374f43d41e7e789822f879968
Alejandro Benitez <edwin.benitez@bizagi.com>
2022-02-27 14:52:29 -0500

    Control TargetSite when serialize to Json, add getSizeInMB, add source as field, control null values in event result, add getByQueryCondition

--- CheckEnvironment/CheckEnvironment/CheckEnvironment/Checker.cs
+++ CheckEnvironment/CheckEnvironment/CheckEnvironment/Checker.cs
@@ -8,8 +8,8 @@ namespace CheckEnvironment
     //@TODO
     //Desactivar / activarlo [OK]
     //Agreagar metodo de limpieza de valores [OK]
-    //Verificar cuantos megas ocupa en memoria la tabla de eventos...
-    //Agregar source como dato opcional
+    //Verificar cuantos megas ocupa en memoria la tabla de eventos [OK]
+    //Agregar source como dato opcional [OK]
     //no repetir valores (tabla valor, tabla instancia de valor...)
     //Metodo para recibir consultas
     //Formatear/formalizar mejor valores de fecha al ser visibles para analisis
@@ -41,7 +41,7 @@ namespace CheckEnvironment
             _connection = new SQLiteConnection("Data Source=:memory:");
             //_connection = new SqliteConnection("Data Source=InMemorySample;Mode=Memory;Cache=Shared");
             _connection.Open();
-            var cmd = new SQLiteCommand("create table event(created datetime, category text, value text)", _connection);
+            var cmd = new SQLiteCommand("create table event(created datetime, category varchar(100), source varchar(200), value text)", _connection);
             cmd.ExecuteNonQuery();
         }
                
@@ -52,12 +52,10 @@ namespace CheckEnvironment
                 return;
             try
             {
-                if (value == null)
-                    value = "value was null";
-                else if (!(value is string))
-                    value = JsonConvert.SerializeObject(value);
-                var cmd = new SQLiteCommand("insert into event values(CURRENT_TIMESTAMP, $category, $value)", GetConnection());
+                value = GetTextValue(value);
+                var cmd = new SQLiteCommand("insert into event values(CURRENT_TIMESTAMP, $category, $source, $value)", GetConnection());
                 cmd.Parameters.AddWithValue("category", category);
+                cmd.Parameters.AddWithValue("source", source);
                 cmd.Parameters.AddWithValue("value", value);
                 cmd.Prepare();
                 cmd.ExecuteNonQuery();
@@ -78,7 +76,7 @@ namespace CheckEnvironment
 
         public static List<Event> GetEventsByCategory(string category)
         {            
-            string stm = "SELECT created, value from event where category = $category";
+            string stm = "SELECT created, category, value, source from event where category = $category";
             try
             {
                 var cmd = new SQLiteCommand(stm, GetConnection());
@@ -89,16 +87,91 @@ namespace CheckEnvironment
                 while (reader.Read())
                 {
                     Event @event = new Event();
-                    @event.Category = category;
-                    @event.Value = reader.GetString(1);
-                    @event.Created = reader.GetDateTime(0);
+                    @event.Created = IsDbNull(reader, 0)? DateTime.MinValue : reader.GetDateTime(0);
+                    @event.Category = IsDbNull(reader, 1)? null: reader.GetString(1);
+                    @event.Value = IsDbNull(reader, 2) ? null : reader.GetString(2);
+                    @event.Source = IsDbNull(reader, 3) ? null : reader.GetString(3);
                     events.Add(@event);
                 }
                 return events;
             }catch (Exception ex)
             {
-                return new List<Event>() { new Event() { Category= "error GetEventsByCategory", Value = JsonConvert.SerializeObject(ex), Created = DateTime.UtcNow } };
+                return new List<Event>() { new Event() { Category= "error GetEventsByCategory", Value = GetTextValue(ex), Created = DateTime.UtcNow } };
             }
+        }
+
+        private static bool IsDbNull(SQLiteDataReader reader, int ordinal)
+        {
+            return reader[ordinal] == DBNull.Value;
+        }
+
+        public static List<Event> GetEventsByQueryCondition(string condition)
+        {
+            string stm = $"SELECT created, category, value, source from event where {condition}";
+            try
+            {
+                var cmd = new SQLiteCommand(stm, GetConnection());                
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                List<Event> events = new List<Event>();
+                while (reader.Read())
+                {
+                    Event @event = new Event();
+                    @event.Created = reader.GetDateTime(0);
+                    @event.Category = reader.GetString(1);
+                    @event.Value = reader.GetString(2);
+                    @event.Source = reader.GetString(3);
+                    events.Add(@event);
+                }
+                return events;
+            }
+            catch (Exception ex)
+            {
+                return new List<Event>() { new Event() {
+                    Category = "error GetEventsByQueryCondition",
+                    Value = GetTextValue(ex),
+                    Created = DateTime.UtcNow,
+                    Source = "Checker"
+                } };
+            }
+        }
+
+        public static double GetMBSizeInMemory()
+        {            
+            string stm = "SELECT SUM(\"pgsize\") FROM \"dbstat\" WHERE name='event'";
+            try
+            {
+                var cmd = new SQLiteCommand(stm, GetConnection());                
+                SQLiteDataReader reader = cmd.ExecuteReader();                
+                double result = -1;
+                while (reader.Read())
+                {
+                    result = reader.GetDouble(0);
+                }
+                return result/(1024*1204);
+            }catch 
+            {
+                return -1;
+            }
+        }
+
+        private static string GetTextValue(object obj)
+        {
+            if (obj == null)
+                return "value was null";
+            else if (obj is Exception)
+                return FormatException(obj);
+            else if (!(obj is string))
+                return JsonConvert.SerializeObject(obj);
+            else
+                return obj.ToString();
+        }
+
+        private static string FormatException(object obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.None, new JsonSerializerSettings
+            {
+                ContractResolver = new JsonIgnoreResolver("TargetSite")//because sqliteException has big and cycle information with this property...
+            });
         }
     }
 
@@ -106,6 +179,7 @@ namespace CheckEnvironment
     {
         public string Category { get; set; }
         public string Value { get; set; }
+        public string Source { get; set; }
         public DateTime Created { get; set; }
     }
 
--- CheckEnvironment/CheckEnvironment/CheckEnvironmentTests/CheckerTest.cs
+++ CheckEnvironment/CheckEnvironment/CheckEnvironmentTests/CheckerTest.cs
@@ -17,12 +17,47 @@ namespace CheckEnvironmentTests
         {
             //When
             Checker.Enabled = true;
-            Checker.RegisterEvent("default", "llego aqui");
+            Checker.RegisterEvent("default", "llego aqui", "CheckerTest.TestStoreEvent");
             //Then
             var result = Checker.GetEventsByCategory("default");
             Assert.IsTrue(result.Count == 1);
             var ev = result[0];
             Assert.AreEqual("llego aqui", ev.Value);
+            Assert.AreEqual("CheckerTest.TestStoreEvent", ev.Source);
+        }
+
+        [Test]
+        public void TestQueryNotCategoryResult()
+        {
+            //When
+            Checker.Enabled = true;
+            Checker.RegisterEvent("default", "llego aqui", "CheckerTest.TestStoreEvent");
+            //Then
+            var result = Checker.GetEventsByCategory("cat");
+            Assert.IsTrue(result.Count == 0);           
+        }
+
+        [Test]
+        public void TestQueryNullCategory()
+        {
+            //When
+            Checker.Enabled = true;
+            Checker.RegisterEvent("default", "llego aqui", "CheckerTest.TestStoreEvent");
+            //Then
+            var result = Checker.GetEventsByCategory(null);
+            Assert.IsTrue(result.Count == 0);
+        }
+
+        [Test]
+        public void TestQueryNullCondition()
+        {
+            //When
+            Checker.Enabled = true;
+            Checker.RegisterEvent("default", "llego aqui", "CheckerTest.TestStoreEvent");
+            //Then
+            var result = Checker.GetEventsByQueryCondition(null);
+            Assert.IsTrue(result.Count == 1);
+            Assert.AreEqual("error GetEventsByQueryCondition", result[0].Category);
         }
 
 
@@ -79,6 +114,33 @@ namespace CheckEnvironmentTests
             //Then
             var result = Checker.GetEventsByCategory("default");
             Assert.IsTrue(result.Count == 0);            
+        }
+
+        [Test]
+        public void TestSizeInMemory()
+        {
+            //When
+            Checker.UpdateEnabled(true);
+            Checker.RegisterEvent("default", "llego aqui");
+           
+            //Then
+            var result = Checker.GetMBSizeInMemory();
+            Assert.AreEqual(0.0033222591362126247d, result);
+        }
+
+        [Test]
+        public void TestSizeInMemoryManyValues()
+        {
+            //When
+            Checker.UpdateEnabled(true);
+            for (int i = 0; i < 1000; i++)
+            {
+                Checker.RegisterEvent("defaultCat", "10/7/2021 4:49:28 PM	[Error]	{\"Message\":\"The given key was not present in the dictionary.\",\"StackTrace\":\"   at BizAgi.Deployment.ExportImportWrapper.Import(CConfiguration configuration, ILightPackage contentSource, LightProgressStatus progress, ImportValidationSettings validateMetadataSettings, Boolean savePackageInformationInTarget)\r\n   at Bizagi.Deployment.RestServices.Import.Execution.ImportBex.RunImport(ParametersExecuteImport paramsImport, AsyncExecutionLightPackage result)\",\"Inner\":{\"Message\":\"The given key was not present in the dictionary.\",\"StackTrace\":\"   at Bizagi.Metadata.Deployment.Import.AdvancedPackageImporter.Import(CConfiguration configuration, ILightPackage contentSource, LightProgressStatus progress, ImportValidationSettings validateMetadataSettings, Boolean savePackageInformationInTarget, Boolean resetCache, String filename, String username)\r\n   at Bizagi.Metadata.Deployment.ExportImport.Import(CConfiguration configuration, ILightPackage contentSource, LightProgressStatus progress, ImportValidationSettings validateMetadataSettings, Boolean savePackageInformationInTarget, Boolean resetCache)\"}}");
+            }            
+
+            //Then
+            var result = Checker.GetMBSizeInMemory();
+            Assert.AreEqual(1.1129568106312293d, result);
         }
     }
 }
